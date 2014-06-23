1db3a2060f2cf9cedacb5f7e0e5c674e6217dbd9
Hugo ThePooN Denizart <thepoon@cartooncraft.fr>
2014-06-23 19:28:03 +0200

    updated spawn location

--- src/fr/cartooncraft/essentials/commands/SpawnCommand.java
+++ src/fr/cartooncraft/essentials/commands/SpawnCommand.java
@@ -1,6 +1,7 @@
 package fr.cartooncraft.essentials.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -12,7 +13,7 @@ public class SpawnCommand extends CCCommand {
 		if(isPlayer(sender)) {
 			Player p = getPlayer(sender);
 			if(p.isOp())
-				p.teleport(Bukkit.getWorld("world").getSpawnLocation());
+				p.teleport(new Location(Bukkit.getWorld("world"), -685.5, 64, 327.5, 90, 0));
 			else
 				p.sendMessage(noPermission);
 		}
